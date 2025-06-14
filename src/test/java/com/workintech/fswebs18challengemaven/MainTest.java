ba4d23ad755ac26332951f3187e2e2bfee4ff618
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-14 15:31:23 +0300

    + 16 Main test passed -- project done

--- src/test/java/com/workintech/fswebs18challengemaven/MainTest.java
+++ src/test/java/com/workintech/fswebs18challengemaven/MainTest.java
@@ -27,7 +27,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
-
 @ExtendWith(ResultAnalyzer2.class)
 public class MainTest {
     @Mock
